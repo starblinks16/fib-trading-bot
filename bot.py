f2e19dd03537bf645fd185d939e2f7318e42a0b3
starblinks16 <anyiamemmanuel16@gmail.com>
2026-06-14 20:22:04 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,18 +1,17 @@
-AAEEH8mFW06rxvLAwVEFMWoRrROyWUCWABU requests
+import requests
 
 BOT_TOKEN = "8517898625:AAEZmk1kSNm82PihpmudLtaRtO6xpRUqw5E"
 CHAT_ID = "7588696401"
 
-message = "✅ Hello starblinks! Your GitHub bot is connected."
-
 url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
 
-requests.post(
+response = requests.post(
     url,
     data={
         "chat_id": CHAT_ID,
-        "text": message
+        "text": "✅ Test message from GitHub"
     }
 )
 
-print("Message sent")
+print(response.status_code)
+print(response.text)
